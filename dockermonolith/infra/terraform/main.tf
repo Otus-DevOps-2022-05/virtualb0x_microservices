d1689c9897a5fb501674b213f22b4a383662ab7d
Anastasiya Lomakova <anastasia.i.lomakova@tele2.ru>
2022-08-10 09:25:51 +0300

    HW 12

--- dockermonolith/infra/terraform/main.tf
+++
@@ -1,7 +0,0 @@
-provider "yandex" {
-  service_account_key_file = var.service_account_key_file
-  // token     = var.token
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.zone
-}
--- dockermonolith/infra/terraform/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "external_ip_address_app" {
-  value = yandex_compute_instance.reddit[*].network_interface.0.nat_ip_address
-}
-output "hostname" {
-  value = yandex_compute_instance.reddit[*].name
-}
